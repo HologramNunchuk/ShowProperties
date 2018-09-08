5a501b76b07902c1f67768ca78c0ef98f8793f77
Jaime <jaimesof82@gmail.com>
2018-09-07 20:35:29 -0700

    Added a function that uses a function to show the properties of the object.

---
+++ index.js
@@ -0,0 +1,43 @@
+const luke = {
+  name: "Luke Skywalker",
+  jedi: true,
+  sith: false,
+  age: 45,
+  saberColor: "Green",
+  jediRank: "Master",
+  sithRank: null
+};
+
+const darthVader = {
+  name: "Darth Vader",
+  jedi: false,
+  sith: true,
+  age: 64,
+  saberColor: "Red",
+  jediRank: null,
+  sithRank: "Lord"
+};
+
+const yoda = {
+  name: "Yoda",
+  jedi: true,
+  sith: false,
+  age: 900,
+  saberColor: "green",
+  jediRank: "Grand Master",
+  sithRank: null
+};
+
+showProperties(yoda);
+
+function showProperties(object) {
+  for (let key in object) {
+    if (
+      typeof object[key] === "string" ||
+      typeof object[key] === "number" ||
+      typeof object[key] === "boolean" ||
+      typeof object[key] === "object"
+    )
+      console.log(key, object[key]);
+  }
+}
